c74ed74d1f771b1c082186aad3f5bc0fef244caa
Qiang Wen <csqiangwen@gmail.com>
2025-06-07 15:02:49 +0800

    Update test_score_group.py

--- test_score_group.py
+++ test_score_group.py
@@ -19,20 +19,17 @@ metric_nima = pyiqa.create_metric('nima-vgg16-ava', device=torch.device('cuda'))
 metric_niqe = pyiqa.create_metric('niqe', device=torch.device('cuda'))
 metric_clip = pyiqa.create_metric('clipiqa', device=torch.device('cuda'))
 
-
 def get_psnr(im1, im2):
     im1_th = torch.from_numpy((im1.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
     im2_th = torch.from_numpy((im2.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
 
     return metric_psnr(im1_th, im2_th)
 
-
 def get_ssim(im1, im2):
     im1_th = torch.from_numpy((im1.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
     im2_th = torch.from_numpy((im2.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
 
     return metric_ssim(im1_th, im2_th)
-
 
 def get_lpips(im1, im2):
     im1_th = torch.from_numpy((im1.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
@@ -51,7 +48,7 @@ def get_clip(im):
     im = torch.from_numpy((im.astype(np.float32) / 255.).transpose(2,0,1)).unsqueeze(0)
     return metric_clip(im)
 
-## The data folders (gt root) will like：
+## The data folders (gt root) will be like：
 ## gt root
 ##      |__ 
 ##          000000_250 (index_amplify ratio)
@@ -63,8 +60,6 @@ def get_clip(im):
 gt_root = '' 
 pred_root = '' # The output of the method
 new_root = '' # During evaluation, the predicted result and its corresponding GT will be presented together along with the amplification ratio.
-
-## If it is too laborious to execute this evaluation, dont worry, we have released all our predicted results on OneDrive.
 
 gt_paths = natsorted(os.listdir(gt_root))
 pred_paths = natsorted(os.listdir(pred_root))
